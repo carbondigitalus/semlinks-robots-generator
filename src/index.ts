976fa1ae720112313fd715bc995547ec714cd45e
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-22 00:49:09 -0400

    update to RobotsGenerator

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,7 @@ import { RobotsGenerator } from './robots';
 
 export default async function robotsGenerator() {
   // execute class for variable usage
-  const robotsParser = new RobotsParser();
-  if (returnType === ReturnType.console) {
-    // console.log method
-    return robotsParser.logFile(filePath);
-  }
-  if (returnType === ReturnType.browser) {
-    // format array method
-    const data = await robotsParser.parseFile(filePath);
-    return data;
-  }
-  return;
+  const generator = new RobotsGenerator();
+  const data = await generator.generateFile();
+  return data;
 }
