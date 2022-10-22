869cdec5d2f25611fb8dbf0fe3ebd39b798278e6
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-22 00:49:03 -0400

    update to RobotsGenerator

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Custom Modules
 import { RobotsGenerator } from './robots';
 
-export default async function robotsParser(returnType: `${ReturnType}`, filePath: string) {
+export default async function robotsGenerator() {
   // execute class for variable usage
   const robotsParser = new RobotsParser();
   if (returnType === ReturnType.console) {
