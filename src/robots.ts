c26c09f20db0a29ad217defe4477f3b9739a043a
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-22 00:59:41 -0400

    add comment, flag methods not completed

--- src/robots.ts
+++ src/robots.ts
@@ -84,7 +84,7 @@ export class RobotsGenerator {
     }
     return { type: LineType.other, content: parsedLine };
   }
-
+  // NOT DONE
   public async generateFile() {
     return new Promise((resolve, reject) => {
       // create empty array for returning data
