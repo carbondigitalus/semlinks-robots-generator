b9097631b2b76cfd8219b69e316d82f52e7f3334
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-23 14:50:45 -0400

    add output path into method param

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { RobotsGenerator } from './robots';
 export default async function robotsGenerator(outPath: string) {
   // execute class for variable usage
   const generator = new RobotsGenerator();
-  const data = await generator.generateFile();
+  const data = await generator.generateFile(outPath);
   return data;
 }
