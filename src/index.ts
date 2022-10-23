8f8adb70d90b450032895a98499088a69011bff6
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-10-23 14:50:57 -0400

    comment out console.log

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export default async function robotsGenerator(outPath: string) {
   // execute class for variable usage
   const generator = new RobotsGenerator();
   const data = await generator.generateFile(outPath);
+  // console.log('robotsGenerator data:\n', data);
   return data;
 }
